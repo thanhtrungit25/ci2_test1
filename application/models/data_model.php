23576d2222b76131ebd3ee37035bad2c9f826903
Thanh Trung <thanhtrung.it25@gmail.com>
2016-06-05 23:36:52 +0700

    Send email width codeigniter.

---
+++ application/models/data_model.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Data_model extends CI_Model
+{
+	function getAll()
+  	{
+  		$q = $this->db->query("SELECT * FROM data");
+
+  		if ($q->num_rows() > 0) {
+  			foreach ($q->result() as $row)	 {
+  				$data[] =  $row;
+  			}
+  		}
+  		return $data;
+	}
+}
